a6d4a8cf3d0aa2dfab788ff441e8e6ef6462f2b7
angeella <a.andreella8@gmail.com>
2020-04-28 18:42:40 +0200

    add

--- Angela/stream_data.R
+++ Angela/stream_data.R
@@ -4,7 +4,9 @@ COVIDrecoveredGlobal <- "https://github.com/CSSEGISandData/COVID-19/tree/master/
   
 COVIDrecoveredUS <- "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 
-COVIDrecoveredGlobal <- "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+COVIDconfirmedGlobal <- "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
 
-read.csv(url(COVIDrecoveredGlobal))
+dataRecoveredGlobal <-read.csv(url(COVIDrecoveredGlobal))
+dataRecoveredUS <-read.csv(url(COVIDrecoveredUS))
+dataRecoveredGlobal <-read.csv(url(COVIDconfirmedGlobal))
