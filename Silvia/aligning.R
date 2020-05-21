8dbe318b6c7562298463afb6ebcdb22b1be17458
silviadenicolo <silviadenicolo@users.noreply.github.com>
2020-05-21 09:56:58 +0200

    add silvia

--- Silvia/aligning.R
+++ Silvia/aligning.R
@@ -1,5 +1,8 @@
 
 
+# alignment at the day of the first contagion (from 10 days before)
+
+
 
 # missing values treatment via spline approx
 
@@ -14,9 +17,10 @@ missval=function(reshline){
 aligning=function(reshaped, fcdays=firstc){
  
    numdb=as.matrix(reshaped[,-1])
-   alignedc=matrix(nrow=28,ncol=138)
-   for (i in 1:28){
-       if(fcdays[i]>25) alignedc[i,c(1:(114-fcdays[i]+25))]=numdb[i, c((fcdays[i]-24):114)]
+   alignedc=matrix(nrow=dim(numdb)[1],ncol=24+dim(numdb)[2])
+   
+   for (i in 1:dim(numdb)[1]){
+       if(fcdays[i]>25) alignedc[i,c(1:(dim(numdb)[2]-fcdays[i]+1+24))]=numdb[i, c((fcdays[i]-24):dim(numdb)[2])]
        else alignedc[i,c((25+1-fcdays[i]):(114+25-fcdays[i]))]=numdb[i,]
      }
     alignedc=alignedc[,c(15:99)] #80 righe (5 giorni prima del contagio + 75 dopo contagio)
